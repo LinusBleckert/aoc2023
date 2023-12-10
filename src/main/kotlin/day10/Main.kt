00e514ada0d7f23496981e052d683b0d12ef58f3
Bleckert <Bleckert@DESKTOP-2H3OF8K>
2023-12-10 11:01:58 +0100

    Day10 more

--- src/main/kotlin/day10/Main.kt
+++ src/main/kotlin/day10/Main.kt
@@ -1,25 +1,57 @@
 package day10
 
+import java.io.File
 
-enum class DIRECTION{
-    SOUTH,
-    EAST,
-    NORTH,
-    WEST
-}
+val filePath = "C:\\Users\\Bleckert\\IdeaProjects\\aoc2023\\src\\main\\kotlin\\day10\\testinput.txt";
 
-fun getNextPositionsFromTile(direction: DIRECTION, ch: Char):List<Pair<Int,Int>>{
+val map = File(filePath).readLines()
+
+
+
+fun getNextPositionsFromTile(ch: Char):List<Pair<Int,Int>>{
 
     // Return all possible stuff, Create a map with all tiles in maze, replace lowest number of steps
     // Loop all
     when(ch) {
-        '|' ->{
-            listOf( Pair(1,0), Pair(-1,0))
-        }
+        '|' ->
+            return listOf(Pair(1,0),Pair(-1,0))
+        '-' ->
+            return listOf(Pair(0,-1),Pair(0,1))
+        'L' ->
+            return listOf(Pair(0,1),Pair(-1,0))
+        'J' ->
+            return listOf(Pair(0,-1),Pair(-1,0))
+        '7' ->
+            return listOf(Pair(0,-1),Pair(1,0))
+        'F' ->
+            return listOf(Pair(0,1),Pair(1,0))
+        else ->
+            return emptyList()
     }
 }
 
+fun findCharPosition(char: Char, stringList: List<String>): Pair<Int, Int>? {
+    stringList.forEachIndexed { rowIndex, str ->
+        str.forEachIndexed { colIndex, currentChar ->
+            if (currentChar == char) {
+                return Pair(rowIndex, colIndex)
+            }
+        }
+    }
+    return null
+}
+
+fun replaceSWithCorrectChar(start:Pair<Int,Int>, map:List<String>){
+
+    // check if surrounding connects?
+    // Loop getNextPositionsFromTile and check which returns our pos?
+
+}
 
 fun main(){
+
+    val startPos = findCharPosition('S', map)
+    println(startPos)
+    // Replace S?
 
 }
