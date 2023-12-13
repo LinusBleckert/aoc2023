ccef02a47682caf3d262b1e0e60ebffc52683948
Bleckert <Bleckert@DESKTOP-2H3OF8K>
2023-12-13 11:34:58 +0100

    Before day 13

--- src/main/kotlin/day12/Main.kt
+++ src/main/kotlin/day12/Main.kt
@@ -26,8 +26,40 @@ fun testString(str: String, list:List<Int>):Int{
     return 1
 }
 
-var cacheMap = HashMap<Pair<String, List<Int>>,Int>();
+var cacheMap = HashMap<Pair<String, List<Int>>,ULong>();
 // Go down path, if
+
+fun getAmountOfArrangementsWithCache(str: String, list:List<Int>):ULong{
+    if (str == "")
+        return if (list.isEmpty()) 1UL else 0UL
+
+    if(list.size == 0)
+        return if ("#" in str) 0UL else 1UL
+
+    if(Pair(str, list) in cacheMap)
+     return cacheMap.get(Pair(str, list))!!
+
+    var result = 0UL
+
+    if(str[0] in ".?")
+        result += getAmountOfArrangementsWithCache(str.drop(1), list)
+    if(str[0] in "#?")
+        if(list[0] <= str.length && '.' !in str.substring(0, list[0]) && (list[0] == str.length || str[list[0]] != '#')){
+            //if(list.size == 1 && (str.length == list[0] || str.length == list[0] + 1))
+            //    result += getAmountOfArrangementsWithCache("" ,list.drop(1))
+            //else{
+            if(list[0] >= str.length)
+                result += getAmountOfArrangementsWithCache("",list.drop(1))
+            else{
+                val newString = str.substring(list[0] + 1,str.length)
+                val droppedFirst = list.drop(1)
+                result += getAmountOfArrangementsWithCache(newString,droppedFirst)
+            }
+            //}
+        }
+    cacheMap[Pair(str, list)] = result
+    return result
+}
 
 // Need to reverse the code kind of
 // If the first part is found, remove it from list and that part of the string aswell
@@ -44,23 +76,10 @@ fun getAmountOfArrangements(str: String, list:List<Int>):Int{
             return 0
     }
     var res = 0
-    if(cacheMap.contains(Pair(str.replaceFirst('?', '#'),list)))
-        res += cacheMap[Pair(str.replaceFirst('?', '#'),list)]!!
-    else{
-        val asd = getAmountOfArrangements(str.replaceFirst('?', '#'),list)
-        cacheMap[Pair(str.replaceFirst('?', '#'),list)] = asd
-        res += asd
-
-    }
-    if(cacheMap.contains(Pair(str.replaceFirst('?', '.'),list)))
-        res += cacheMap[Pair(str.replaceFirst('?', '.'),list)]!!
-    else{
-        val asd = getAmountOfArrangements(str.replaceFirst('?', '.'),list)
-        cacheMap[Pair(str.replaceFirst('?', '.'),list)] = asd
-        res += asd
-    }
-    //val oneSide = getAmountOfArrangements(str.replaceFirst('?', '#'),list)
-    //val otherSide = getAmountOfArrangements(str.replaceFirst('?', '.'),list)
+    val oneSide = getAmountOfArrangements(str.replaceFirst('?', '#'),list)
+    val otherSide = getAmountOfArrangements(str.replaceFirst('?', '.'),list)
+    res += oneSide
+    res += otherSide
     return res
 }
 
@@ -74,8 +93,6 @@ fun main(){
         println(result)
     }
 
-    //ManualTesting()
-    //return
     //val str = "#.#.###"
     //val list = listOf(1,1,3)
     //println(testString(str, list))
@@ -83,28 +100,17 @@ fun main(){
     // ???.### 1,1,3
     fun Part1(): ULong{
         return map.fold(0UL) { acc, value ->
-
             val split = value.split(" ")
             //val normalStr = split[0]
-            //val strEndQM = split[0] + '?'
-            val strStartQM = split[0]
-            val str = strStartQM// + strStartQM + strStartQM+ strStartQM+ split[0]
+            var str = split[0] + '?'
+            str = str + str + str + str + split[0]
             val list = split[1].split(',').map { it.toInt() }
-            val list3 = list// + list + list + list + list
+            val list3 = list + list + list + list + list
             var result = getAmountOfArrangements(str, list3)
-            //var resultStart = getAmountOfArrangements(strStartQM, list3)
-            //var result = 0UL
-            /*if(resultEnd > resultStart)
-                result = resultEnd.toULong()
-            else
-                result = resultStart.toULong()
-              */
-            // Add ? to end or start, get wahtever is highest and multply by 4?
-            //result = result*result*result*result
-            //result *= getAmountOfArrangements(normalStr, list).toULong()
-            cacheMap.clear()
+            //cacheMap.clear()
             println(result)
             result.toULong() + acc
+
         }
     }
 
